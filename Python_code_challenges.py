73cfad29fe5e07f42e373114b083c0789a6e419f
ashenell <79301820+ashenell@users.noreply.github.com>
2022-04-05 15:16:00 +0300

    Update Python_code_challenges.py

--- Python_code_challenges.py
+++ Python_code_challenges.py
@@ -15,3 +15,21 @@ calca = instert_radian * (180 / pi)
 
 print(calca) #Test
 """
+'''
+#Sort a list to asc/desc if input does not meat the value it prints out lists
+list_nums = [5, 6, 7 ,4 ,8 ,9 , 1, 3] #Store list of random numbesrs
+values = ''
+value1 = 'asc'
+value2 = 'desc'
+value3 = 'none'
+values = input('How you wanna sort the list(asc)(desc)(none): ')
+
+if values == value1:
+    list_nums.sort()
+    print(list_nums)
+elif values == value2:
+    list_nums.sort(reverse=True)
+    print(list_nums)
+else:
+    print(list_nums)    
+'''
