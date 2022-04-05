c97d63f51480717d3944a94a483c405fb91a1bd7
ashenell <79301820+ashenell@users.noreply.github.com>
2022-04-05 15:21:02 +0300

    Update Python_code_challenges.py

--- Python_code_challenges.py
+++ Python_code_challenges.py
@@ -1,35 +1,40 @@
 
 """
+
 from math import pi
-#Convert radians into degrees
+def function_conver():
+    #Convert radians into degrees
 
-instert_radian = ''
-while instert_radian is not int:
-    try:
-        instert_radian = int(input('Enter numeric radian value: '))
-        break
-    except ValueError:
-        print('Value is not correct!')
-            
-calca = instert_radian * (180 / pi)
+    instert_radian = ''
+    while instert_radian is not int:
+        try:
+            instert_radian = int(input('Enter numeric radian value: '))
+            break
+        except ValueError:
+            print('Value is not correct!')
 
-print(calca) #Test
+    calca = instert_radian * (180 / pi)
+
+    print(calca) #Test
+functuin_convert()
 """
 '''
 #Sort a list to asc/desc if input does not meat the value it prints out lists
-list_nums = [5, 6, 7 ,4 ,8 ,9 , 1, 3] #Store list of random numbesrs
-values = ''
-value1 = 'asc'
-value2 = 'desc'
-value3 = 'none'
-values = input('How you wanna sort the list(asc)(desc)(none): ')
+def function_sort():
+    list_nums = [5, 6, 7 ,4 ,8 ,9 , 1, 3] #Store list of random numbesrs
+    values = ''
+    value1 = 'asc'
+    value2 = 'desc'
+    value3 = 'none'
+    values = input('How you wanna sort the list(asc)(desc)(none): ')
 
-if values == value1:
-    list_nums.sort()
-    print(list_nums)
-elif values == value2:
-    list_nums.sort(reverse=True)
-    print(list_nums)
-else:
-    print(list_nums)    
+    if values == value1:
+        list_nums.sort()
+        print(list_nums)
+    elif values == value2:
+        list_nums.sort(reverse=True)
+        print(list_nums)
+    else:
+        print(list_nums)    
+function_sort()   
 '''
